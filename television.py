683ab13aebdef41bcbf85506d3202a104ffdfabe
alasiafps <167499148+alasiafps@users.noreply.github.com>
2024-04-19 12:15:41 -0500

    Update television.py

--- television.py
+++ television.py
@@ -4,49 +4,85 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        method to set the default status variables of the tv
+        :return: None
+        """
         self.__status = False
         self.__muted = False
         self.__volume = self.MIN_VOLUME
         self.__channel = self.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        """
+        method to turn the tv power on or off
+        :return: None
+        """
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Method to mute the volume on the tv.
+        :return: None
+        """
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Method to change the TV channel up by one. It does not
+        go above the MAX_CHANNEL variable. Instead, if you are at max channel, it resets to min channel
+        :return: None
+        """
         if self.__status:
             if self.__channel == self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Method to change the TV channel down by one. It does not go below the MIN_CHANNEL
+        Variable. Instead, if you are at min channel, it jumps to max channel
+        :return:
+        """
         if self.__status:
             if self.__channel == self.MIN_CHANNEL:
                 self.__channel = self.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Method that increases the volume by one. If you are at max_volume and the method is called,
+        nothing happens.
+        :return: None
+        """
         if self.__status:
             if self.__volume < self.MAX_VOLUME:
                 self.__volume += 1
             if self.__muted:
                 self.__muted = False
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Method that decreases the volume by one. If you are at min_volume, and the method is called,
+        nothing happens.
+        :return:
+        """
         if self.__status:
             if self.__volume > self.MIN_VOLUME:
                 self.__volume -= 1
             if self.__muted:
                 self.__muted = False
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Method to show status of the TV.
+        :return: tv status
+        """
         if self.__muted:
-            return f"Power=[{self.__status}], Channel=[{self.__channel}], Volume=[0]"
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = 0"
         else:
-            return f"Power=[{self.__status}], Channel=[{self.__channel}], Volume=[{self.__volume}]"
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
